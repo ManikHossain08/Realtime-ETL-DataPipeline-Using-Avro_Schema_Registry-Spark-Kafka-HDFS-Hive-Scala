64b308055e7b3a86e5cc4c6311e9352283d56bf1
ManikHossain08 <manik083017@gmail.com>
2021-11-03 20:06:43 -0400

    refactor project and clean

--- src/main/scala/com/etl/spark/streaming/scala/AvroConsume.scala
+++ src/main/scala/com/etl/spark/streaming/scala/AvroConsume.scala
@@ -2,34 +2,28 @@ package com.etl.spark.streaming.scala
 
 import io.confluent.kafka.serializers.{AbstractKafkaSchemaSerDeConfig, KafkaAvroDeserializer}
 import org.apache.avro.generic.GenericRecord
-
 import java.util.Properties
 import org.apache.kafka.clients.consumer.ConsumerConfig._
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.common.serialization.StringDeserializer
-
 import java.time.Duration
 import scala.collection.JavaConverters._
 
 object AvroConsume extends App {
 
-  // 1. prepare Kafka consumer properties
   val consumerProperties = new Properties()
   consumerProperties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-  consumerProperties.setProperty(GROUP_ID_CONFIG, "movie-consumer1")
+  consumerProperties.setProperty(GROUP_ID_CONFIG, "trip-consumer")
   consumerProperties.setProperty(KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
-  /* What is this configuration for ('auto.offset.reset')? */
-  consumerProperties.setProperty(AUTO_OFFSET_RESET_CONFIG, "earliest")
+  consumerProperties.setProperty(AUTO_OFFSET_RESET_CONFIG, "latest")
   consumerProperties.setProperty(VALUE_DESERIALIZER_CLASS_CONFIG, classOf[KafkaAvroDeserializer].getName)
   consumerProperties.setProperty(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081")
 
-  // 2. instantiate a Kafka consumer
-  val consumer = new KafkaConsumer[String, GenericRecord](consumerProperties)
-  consumer.subscribe(List("test3_enriched_trip").asJava)
-
+  val avroConsumer = new KafkaConsumer[String, GenericRecord](consumerProperties)
+  avroConsumer.subscribe(List("bdsf2001_manik_enriched_trip").asJava)
 
   while(true) {
-    val polledRecords = consumer.poll(Duration.ofSeconds(1))
+    val polledRecords = avroConsumer.poll(Duration.ofSeconds(1))
     if (!polledRecords.isEmpty) {
       val recordIterator = polledRecords.iterator()
       while(recordIterator.hasNext) {
